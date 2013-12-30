127b0354777494d3af7695fcf44e18e099540743
Francois-Rene Rideau <tunes@google.com>
2013-12-30 03:15:29 -0500

    Export install-all-quicklisp-provided-systems

--- package.lisp
+++ package.lisp
@@ -1,4 +1,5 @@
 (cl:defpackage :ql-test
   (:use :cl :inferior-shell :asdf/driver :fare-utils :lisp-invocation)
   (:export
+   #:install-all-quicklisp-provided-systems
    #:test-all-quicklisp-systems))
--- ql-test.lisp
+++ ql-test.lisp
@@ -18,14 +18,20 @@
   (let ((output (subpathname *ql-test-directory* (asdf:coerce-name system) :type "log")))
     (ensure-directories-exist output)
     (run
-     `((> ,output) (>& 2 1) (<& -)
+     `((> ,output) (>& 2 1) (< /dev/null)
        ,@(test-system-command system :implementation-type implementation-type))
      :on-error nil)))
 
 (defun quicklisp-provided-sytems ()
   (ql-dist:provided-systems (ql-dist:dist "quicklisp")))
 
+(defun quicklisp-provided-system-names ()
+  (mapcar #'ql-dist:name (quicklisp-provided-sytems)))
+
+(defun install-all-quicklisp-provided-systems ()
+  (map () 'ql-dist:ensure-installed (quicklisp-provided-sytems)))
+
 (defun test-all-quicklisp-systems (&key from)
-  (let ((all-systems (mapcar #'ql-dist:name (quicklisp-provided-sytems))))
+  (let ((all-systems (quicklisp-provided-system-names)))
     (map () #'test-system
          (if from (member from all-systems :test 'equal) all-systems))))
